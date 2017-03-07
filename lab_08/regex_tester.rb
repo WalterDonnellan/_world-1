c8e4df55d509114b9709bfafa064f64d2753ecba
Alina Yu <AlinaYu@Alinas-MacBook-Air.local>
2017-03-06 19:45:12 -0500

    Added tests for methods

--- lab_08/regex_tester.rb
+++ lab_08/regex_tester.rb
@@ -12,4 +12,10 @@ class RegexTester
     def statement
         @statement
     end
-  end
+end
+
+regex = RegexTester.new
+regex.pattern = /^(http:\/\/)?www\.\w+\.(com|edu|org)$/  # from test_arrays.rb
+puts regex.pattern
+regex.statement = "http://www.google.com"
+puts regex.statement
